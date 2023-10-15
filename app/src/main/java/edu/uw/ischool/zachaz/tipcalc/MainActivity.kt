fb667adf25bf53bbd2969f205cf10ff37da62c3b
freymond <37636251+freymend@users.noreply.github.com>
2023-10-14 18:03:02 -0700

    create tipcalc project

---
+++ app/src/main/java/edu/uw/ischool/zachaz/tipcalc/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.uw.ischool.zachaz.tipcalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
